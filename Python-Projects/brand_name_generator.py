2f22db2f6ebe8a7db19396fbc9131f0b80e1eb2d
nikhilrattu <69786938+nikhilrattu@users.noreply.github.com>
2026-04-15 15:35:25 +0530

    Add files via upload

---
+++ Python-Projects/brand_name_generator.py
@@ -0,0 +1,6 @@
+# This is a brand name generator prjoect
+print("Thank you for using the brand name generator.\n")
+print("I can suggest you the name based on following questions\n")
+city=input("Which city did you grow up in?\n")
+pet=input("What's the name of your pet?\n")
+print("the name of your band is " + city +" "+ pet)
